5cc3c7ee2e8e7042e7adf02ae36dc17174f94177
bentshib <130330735+bentshib@users.noreply.github.com>
2024-06-14 13:39:15 -0400

    Add files via upload

---
+++ Python code/Python code 1.py
@@ -0,0 +1,364 @@
+Python 3.12.2 (v3.12.2:6abddd9f6a, Feb  6 2024, 17:02:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> 
+>>> 
+>>> print("Ben is learning python, and people keep bothering him, hummm crazyyy!")
+Ben is learning python, and people keep bothering him, hummm crazyyy!
+>>> a=3
+>>> b=7
+>>> c-a+b
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    c-a+b
+NameError: name 'c' is not defined
+>>> c-a+b
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    c-a+b
+NameError: name 'c' is not defined
+>>> c-a+b
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    c-a+b
+NameError: name 'c' is not defined
+>>> a=3
+>>> b=9
+>>> c=a+b
+>>> print(c)
+12
+>>> help
+Type help() for interactive help, or help(object) for help about object.
+>>> help()
+Welcome to Python 3.12's help utility! If this is your first time using
+Python, you should definitely check out the tutorial at
+https://docs.python.org/3.12/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To get a list of available
+modules, keywords, symbols, or topics, enter "modules", "keywords",
+"symbols", or "topics".
+
+Each module also comes with a one-line summary of what it does; to list
+the modules whose name or summary contain a given string such as "spam",
+enter "modules spam".
+
+To quit this help utility and return to the interpreter,
+enter "q" or "quit".
+
+help> 
+
+You are now leaving help and returning to the Python interpreter.
+If you want to ask for help on a particular object directly from the
+interpreter, you can type "help(object)".  Executing "help('string')"
+has the same effect as typing a particular string at the help> prompt.
+>>> 
+>>> 
+>>> has the same effect as typing a particular string at the help> prompt.
+SyntaxError: invalid syntax
+>>> 
+>>> print("today is my second day of learning python")
+today is my second day of learning python
+>>> print(" I know thatit is not going to be easy but by the end of the day, I will make it happen")
+ I know thatit is not going to be easy but by the end of the day, I will make it happen
+>>> print("my mom is going well and I am happy")
+my mom is going well and I am happy
+>>> my mom is going well and I am happy
+SyntaxError: invalid syntax
+
+
+
+print("me and the other on, hummm idk")
+me and the other on, hummm idk
+me and the other on, hummm idk
+SyntaxError: invalid syntax
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Var_a=101
+print("Var_a")
+Var_a
+Var_a
+101
+print(type(Var_a))
+<class 'int'>
+
+type(Var_a)
+<class 'int'>
+type(13)
+<class 'int'>
+type(123.04)
+<class 'float'>
+a=4
+float(a)
+4.0
+b=45.65
+int(b)
+45
+b=99.99
+int(b)
+99
+c=98j
+int(c)
+Traceback (most recent call last):
+  File "<pyshell#63>", line 1, in <module>
+    int(c)
+TypeError: int() argument must be a string, a bytes-like object or a real number, not 'complex'
+c=j99
+Traceback (most recent call last):
+  File "<pyshell#64>", line 1, in <module>
+    c=j99
+NameError: name 'j99' is not defined
+a=1j
+int(1)
+1
+a=1
+complex(a)
+(1+0j)
+
+ben=human
+Traceback (most recent call last):
+  File "<pyshell#70>", line 1, in <module>
+    ben=human
+NameError: name 'human' is not defined
+Ben="human"
+print(Ben)
+human
+
+Var_c="python is a good language"
+print(Var_c)
+python is a good language
+
+type(Var_c)
+<class 'str'>
+
+a=10
+b=2
+c=a+b
+print(c)
+12
+
+a=34
+b=2
+c=a/b
+print(c)
+17.0
+# this is the addition operator
+
+# this is the addition operator
+# let us use flloe concept
+
+a=34
+b=2
+c=a//b
+print(c)
+17
+
+
+a=12
+b=2
+c=a/b
+print(c)
+6.0
+# multiplication
+
+a=12
+c=2
+b=a*c
+print(b)
+24
+
+ben=34
+mounir=3
+rahul=ben/mounir
+print("rahul")
+rahul
+
+a=34
+b=3
+c=a//b
+print(c)
+11
+
+#exponation
+
+
+Var_c=2*
+SyntaxError: invalid syntax
+var_a=2**
+SyntaxError: invalid syntax
+var_a=2**
+SyntaxError: invalid syntax
+
+
+var_a=**2
+SyntaxError: invalid syntax
+var_c=2*
+SyntaxError: invalid syntax
+
+
+x1="my mom is sick"
+x2=" I need to take her to the hospital"
+x3=" but I do not have a car, so what to do?"
+print(x1,x2,x3)
+my mom is sick  I need to take her to the hospital  but I do not have a car, so what to do?
+print(x1, x2, x3)
+my mom is sick  I need to take her to the hospital  but I do not have a car, so what to do?
+print("x1, x2, x3")
+x1, x2, x3
+
+type(x1,x2,x3)
+Traceback (most recent call last):
+  File "<pyshell#139>", line 1, in <module>
+    type(x1,x2,x3)
+TypeError: type.__new__() argument 2 must be tuple, not str
+type(x1)
+<class 'str'>
+
+# string concept
+
+
+
+# indexing
+
+ax="Google colan"
+print(ax)
+Google colan
+print(ax[5])
+e
+print(ax[6])
+ 
+print(xa[7])
+Traceback (most recent call last):
+  File "<pyshell#152>", line 1, in <module>
+    print(xa[7])
+NameError: name 'xa' is not defined. Did you mean: 'a'?
+
+ax="Google Colan"
+print(ax[7])
+C
+print(ax[0])
+G
+print(ax[-1])
+n
+print(ax[-3])
+l
+
+# string slising
+
+# string slising( cutting part of the work from sentences
+
+ax="Google Colan"
+print(ax[0:5])
+Googl
+print(ax[:5])
+Googl
+print(ax[5:])
+e Colan
+print(ax[:])
+Google Colan
+print(ax[:7])
+Google 
+print(ax[8])
+o
+print(ax[:8])
+Google C
+print(ax[8:])
+olan
+
+# exponentiation
+ad1=2*2
+print(ad1)
+4
+ad1=2**2
+print(ad1)
+4
+b=2**3
+print(b)
+8
+b=2*3
+print(b)
+6
+q12= "we are doing hands on practice"
+print(q12[13:])
+hands on practice
+print(q12[13:])
+hands on practice
+hands on practice
+SyntaxError: invalid syntax
+print(q12[:13])
+we are doing 
+print(q12[:])
+we are doing hands on practice
+we are doing hands on practice
+SyntaxError: invalid syntax
+
+
+# concatenation concept
+
+a_1=" we are doing practice"
+a_2=" python"
+print(result)
+Traceback (most recent call last):
+  File "<pyshell#196>", line 1, in <module>
+    print(result)
+NameError: name 'result' is not defined
+results=a_1+a_2
+print(results)
+ we are doing practice python
+results=a_1+"of"=a_2
+SyntaxError: cannot assign to expression
+results=a_1+"of"+a+2
+Traceback (most recent call last):
+  File "<pyshell#200>", line 1, in <module>
+    results=a_1+"of"+a+2
+TypeError: can only concatenate str (not "int") to str
+
+a= " we are practicing"
+b="python"
+results=a+"of"=b
+SyntaxError: cannot assign to expression
+results=a+"of"+b
+print(results)
+ we are practicingofpython
+results=a+ "of" +b
+print(results)
+ we are practicingofpython
+results=a+ " of "+b
+print(results)
+ we are practicing of python
+
+# casting which is converting strings into integer
+ben_1=45
+ben_2=" we are learning big data for"
+ben_3="Days"
+print(ben_2 + str(ben_1) + ben_3)
+ we are learning big data for45Days
+print(ben_2 + str( ben_1 ) + ben_3 )
+ we are learning big data for45Days
+ben_a= 45
+ben_b= " we are learning big data for"
+ben_c=" Days"
+print(ben_b + str(ben_a) + ben_c)
+ we are learning big data for45 Days
+print(str(ben_a) + ben_b + ben_c)
+45 we are learning big data for Days
+
+
